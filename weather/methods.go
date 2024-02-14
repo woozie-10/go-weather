191b94420b30fc701580769d959a9334e3645bcb
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-02-14 12:24:10 +0200

    added GetForecast method

--- weather/methods.go
+++ weather/methods.go
@@ -32,4 +32,21 @@ func (wc *WeatherClient) GetCurrent(city string) (CurrentWeather, error) {
 
 }
 
+func (wc *WeatherClient) GetForecast(city string, days int) (ForecastWeather, error) {
+	requestURL := fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=%s&days=%d", wc.API_KEY, city, days)
+	res, err := http.Get(requestURL)
+	if err != nil {
+		return ForecastWeather{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return ForecastWeather{}, fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+	var forecast ForecastWeather
+	err = json.NewDecoder(res.Body).Decode(&forecast)
+	if err != nil {
+		return ForecastWeather{}, err
+	}
+	return forecast, nil
 
+}
